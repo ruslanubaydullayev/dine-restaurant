93204f32d9351da631105839fa90008bb57982a9
Jamshid <jamshid.oa4k@gmail.com>
2023-03-26 17:31:45 +0500

    first section done

---
+++ S2-tailwind-config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+    theme: {
+      container: {
+        center: true,
+      },
+      screens: {
+        mobile: "640px",
+        tablet: "960px",
+        desktop: "1110px",
+      },
+      fontFamily: {
+        sans: [
+          "Inconsolata",
+          "monospace",
+          "Josefin Sans",
+          "sans-serif",
+          "League Spartan",
+          "Outfit",
+          "Roboto",
+        ],
+      },
+    },
+  };
