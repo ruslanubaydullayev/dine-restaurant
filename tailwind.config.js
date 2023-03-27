73eabb4069b147eb052c5c919af794153f70409e
Abdukhamid <Abdukhamid000@gmail.com>
2023-03-27 12:46:46 +0500

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,23 @@ tailwind.config = {
     container: {
       center: true,
     },
+
     screens: {
       mobile: "640px",
       tablet: "960px",
       desktop: "1110px",
     },
+
+    colors: {
+      beaver: "#9e7f66",
+      codGray: "#111111",
+      mirage: "#17192b",
+      ebonyClay: "#242b37",
+      shuttleGray: "#5c6779",
+    },
+
     fontFamily: {
-      sans: [
-        "Inconsolata",
-        "monospace",
-        "Josefin Sans",
-        "sans-serif",
-        "League Spartan",
-        "Outfit",
-        "Roboto",
-      ],
+      sans: ["League Spartan", "sans-serif"],
     },
   },
 };
